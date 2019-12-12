a43b21dcaf636ed67a18c9b437ff531fe55d85bd
jgerstein <jgerstein@ucvts.tec.nj.us>
2019-12-12 12:56:50 -0500

    Update minimal.py

--- minimal.py
+++ minimal.py
@@ -11,6 +11,7 @@ screen = pygame.display.set_mode( (400, 300) )
 while True:
     # do something for each event in the event queue (list of things that happen)
     for event in pygame.event.get():
+        print(event)
         # Check to see if the current event is a QUIT event
         if event.type == pygame.QUIT:
             # If so, exit the program
