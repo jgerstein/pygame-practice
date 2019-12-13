e0786d6bb5dbcf76a49b3adc8e8abaa3b0d1cf61
jgerstein <jgerstein@ucvts.tec.nj.us>
2019-12-13 10:46:14 -0500

    created color fade

--- colors.py
+++ colors.py
@@ -5,6 +5,14 @@ import random
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
 
+# colors
+red = pygame.Color(255, 0, 0)
+orange = pygame.Color(255, 128, 0)
+yellow = pygame.Color('#ffff00')
+green = pygame.Color('#00ff00')
+c = pygame.Color(0, 0, 255)
+change = 1
+
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 300) )
 
@@ -16,3 +24,16 @@ while True:
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
+    print(c.r)
+    if random.random() < .05:
+        c.r += change
+        c.b -= change
+    if c.r >= 255:
+        c.r = 255
+        change = -abs(change)
+    elif c.r <= 0:
+        c.r = 0
+        change = abs(change)
+    screen.fill( c )
+
+    pygame.display.flip()
